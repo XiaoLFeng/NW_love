9917d19fd60b2b6ee9fce50d3fa4199522b71f1c
XiaoLFeng <gm@x-lf.cn>
2023-02-03 11:30:11 +0800

    简单新增

--- tools/the_first.php
+++ tools/the_first.php
@@ -1,1 +1,69 @@
 <?php
+// 载入
+include_once $_SERVER['DOCUMENT_ROOT'].'/modules/header-loader.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/modules/Functions.php';
+$F = new Functions();
+
+/**
+ * @var array $Normal
+ * @var array $config
+ */
+
+$page = 3;
+
+// MissYou API
+$ApiMiss_url = $F->Current_HTTP().'/api/tools/miss_you_logs.php?type=all&session='.$config['SESSION'];
+$ApiMiss_ch = curl_init($ApiMiss_url);
+curl_setopt($ApiMiss_ch,CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT']);
+curl_setopt($ApiMiss_ch, CURLOPT_RETURNTRANSFER, true);
+$ApiMiss = curl_exec($ApiMiss_ch);
+$ApiMiss = json_decode($ApiMiss,true);
+?>
+<!doctype html>
+<html lang="zh-cn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $Normal['data']['web_title']['data'] ?> - <?php echo $Normal['data']['web_desc']['data'] ?></title>
+    <link rel="icon" href="<?php echo $Normal['data']['web_icon']['data'] ?>">
+    <meta name="description" content="<?php echo $Normal['data']['web_desc']['data'] ?>">
+    <meta name="keywords" content="<?php echo $Normal['data']['web_keyword'] ?>">
+    <meta name="full-screen" content="yes"><!--UC强制全屏-->
+    <meta name="browsermode" content="application"><!--UC应用模式-->
+    <meta name="x5-fullscreen" content="true"><!--QQ强制全屏-->
+    <meta name="x5-page-mode" content="app"><!--QQ应用模式-->
+    <!-- CSS -->
+    <link rel="stylesheet" href="/sources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://at.alicdn.com/t/c/font_3866622_ur298ayiu2.css">
+    <link rel="stylesheet" href="/sources/icons/bootstrap-icons.css">
+</head>
+<body style="background-color: rgba(255,192,203,0.25)">
+<!-- 页首 -->
+<?php include $_SERVER['DOCUMENT_ROOT']."/modules/header.php"; ?>
+<!-- 页中 -->
+<div class="container">
+    <div class="row text-center">
+
+    </div>
+</div>
+<!-- Toast -->
+<div class="toast-container position-fixed top-0 start-0 p-3">
+    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+            <i class="bi bi-info-circle"></i><strong class="me-auto">提醒</strong>
+            <small>现在</small>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body" id="ajax_return"></div>
+    </div>
+</div>
+<!-- 底部菜单 -->
+<?php include $_SERVER['DOCUMENT_ROOT']."/modules/menu.php"; ?>
+</body>
+<script type="text/javascript" src="/sources/js/jquery.min.js"></script>
+<script type="text/javascript" src="/sources/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/sources/js/bootstrap.bundle.min.js"></script>
+
+</html>
